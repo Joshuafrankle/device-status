ddabed7f7792c295fdd0c9274d7b86b995db7472
Joshuafrankle <joshuafrankle7@gmail.com>
2021-08-16 12:38:10 +0530

    Updates

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,22 +1,29 @@
 import { useState, useEffect } from "react";
 import Loader from "../components/Loader";
 import axios from "axios";
-import Img from "../assets/images/server.png";
+import Cards from "./Cards";
+// import Img from "../assets/images/server.png";
 
 export default function LandingPage() {
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([
+    {
+      host: "myself",
+      status: "offline",
+      "ip address": "121121341",
+    },
+  ]);
 
   useEffect(() => {
     axios
       .get("http://opencloud-dev.herokuapp.com")
       .then((res) => {
         setData(res.data["device status"]);
+        console.log(data);
         setLoading(false);
       })
       .catch((error) => {
         console.log(error);
-        setLoading(false);
       });
   }, []);
 
@@ -27,23 +34,17 @@ export default function LandingPage() {
       ) : (
         <>
           <section className="landing-main">
-            {data.map((data, id) => (
-              <div className="card mb-3" key={id}>
-                <div className="card-body">
-                  <img src={Img} alt="image" />
-                  <div className="label-section fw-bold">
-                    <p> Hostname:</p>
-                    <p> Status:</p>
-                    <p> Address:</p>
-                  </div>
-                  <div className="details-section">
-                    <p>{data.host}</p>
-                    <p>{data.status}</p>
-                    <p>{data["ip address"]}</p>
-                  </div>
+            {data.map((data, id) =>
+              data.status === "offline" ? (
+                <div className="offline-card" key={id}>
+                  <Cards data={data} />
                 </div>
-              </div>
-            ))}
+              ) : (
+                <div className="" key={id} style={{ width: "100%" }}>
+                  <Cards data={data} />
+                </div>
+              )
+            )}
           </section>
         </>
       )}
