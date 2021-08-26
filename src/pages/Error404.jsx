cd2d6c3048a26742f8c33e505c171e3d9af8eef2
Joshuafrankle <joshuafrankle7@gmail.com>
2021-08-26 12:46:43 +0530

    Removed class

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -5,12 +5,10 @@ export default function Error404() {
     <>
       <section className="error-main">
         <div>
-          <div className="error-glass text-center">
+          <div className="error-card text-center">
             <img className="error-img mb-4" src={Warning} alt="Done" />
-            <h3 className="text-white">Error 404</h3>
-            <h4 className="text-white mt-4">
-              The Requested page doesn't exist
-            </h4>
+            <h3>Error 404</h3>
+            <h4 className="mt-4">The Requested page doesn't exist</h4>
           </div>
         </div>
       </section>
